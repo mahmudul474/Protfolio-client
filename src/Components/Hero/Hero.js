bc7fd77b2afffc93a2406ae06b96114afa30b3ce
unknown <mahmudul474>
2023-07-21 02:44:45 +0600

    done

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -5,6 +5,16 @@ import reactgif from "../../assates/react.gif";
 import frontent from "../../assates/frontent.gif";
 import nodegif from "../../assates/node.gif";
 import mongogit from "../../assates/mongo.gif";
+import { BsLinkedin } from "react-icons/bs";
+import { AiFillGithub } from "react-icons/ai";
+import JavaScriptImg from "../../assates/js.png";
+import tsImg from "../../assates/ts.png";
+import reactImg from "../../assates/react.png";
+import reduxImg from "../../assates/redux.png";
+import mongoImg from "../../assates/mongodb.png";
+import nodeImg from "../../assates/node.png";
+import nextImg from "../../assates/next.png";
+
 export default function Hero() {
   return (
     <div
@@ -13,19 +23,48 @@ export default function Hero() {
       }}
       className="bg-cover bg-no-repeat bg-center  overflow-hidden"
     >
-      <section className="dark:bg-gray-800 h-screen dark:text-gray-100">
+      <section className="dark:bg-gray-800 h-[800px] dark:text-gray-100">
         <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
           <div className="     flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-            <h1 className="text-5xl font-bold leadi sm:text-6xl">
-              Ac mattis
-              <span className="dark:text-violet-400">senectus</span>erat
-              pharetra
+            <h1 className=" text-lg font-bold    lg:text-6xl">
+              Full-Stack MERN Developer
             </h1>
-            <p className="mt-6 mb-8 text-lg sm:mb-12">
-              Dictum aliquam porta in condimentum ac integer
-              <br className="hidden md:inline lg:hidden" />
-              turpis pulvinar, est scelerisque ligula sem
+            <p className="mt-6 mb-8  font-semibold text-lg sm:mb-12">
+              Hello! I'm Mahmudul Hasan, a dedicated MERN full-stack developer
+              based in Dhaka, Bangladesh.
             </p>
+
+            <div className="flex justify-start items-center">
+              <span className="text-3xl  mr-3">
+                <a
+                  href="https://www.linkedin.com/in/mahmudul474"
+                  target="_blank"
+                >
+                  <BsLinkedin></BsLinkedin>
+                </a>
+              </span>
+              <span className="text-3xl ">
+                <a href="https://github.com/mahmudul474" target="_blank">
+                  <AiFillGithub></AiFillGithub>
+                </a>
+              </span>
+            </div>
+
+            <div className="flex justify-start mt-8  items-center">
+              <h2 className="text-2xl font-semibold">Tech Stack</h2>
+              <div className="flex justify-start   items-center">
+                <img
+                  className="w-16  h-16 object-cover mr-2"
+                  src={JavaScriptImg}
+                />
+                <img className="w-12 h-10 object-cover mr-2" src={tsImg} />
+                <img className="w-12 h-10 object-cover mr-2" src={reactImg} />
+                <img className="w-12 h-10 object-cover mr-2" src={reduxImg} />
+                <img className="w-12 h-10 object-cover mr-2" src={nextImg} />
+                <img className="w-12 h-10 object-cover mr-2" src={nodeImg} />
+                <img className="w-12 h-10 object-cover mr-2" src={mongoImg} />
+              </div>
+            </div>
           </div>
           <div className="     h-full   p-6 mt-8   relative ">
             <img
